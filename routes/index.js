cc69e093d91b9f3e5cca11b13820cc312337f5cd
Yasmini Gyawali <gyawaliyasmini246@gmail.com>
2022-10-21 22:07:44 +0545

    NotesUploader Project

--- routes/index.js
+++ routes/index.js
@@ -26,9 +26,9 @@ const upload = multer({
 router.get('/',function(req,res,next){
   res.render('home',{List:notes})
 })
-router.get('/Note',function(req,res,next){
-   res.render('Note',{title: 'Notess'})
- })
+// router.get('/Note',function(req,res,next){
+//    res.render('Note',{title: 'Notess'})
+//  })
  router.get('/upload',function(req,res,next){
   res.render('Upload',{title: 'Uploads'})
 })
@@ -38,12 +38,48 @@ router.post('/upload1',upload.single('image'), async (req,res,next) => {
   await notes.insertMany([{
     subject: req.body.subject, 
     Semesters: req.body.Semesters, 
-    pdfPath: req.file.image}])
+    pdfPath: req.body.subject + path.extname(req.file.originalname)}])
 res.redirect('/');
 })
 
-router.get('/notes', async function(req, res, next){
-  const allNotes = await notes.find();
-  res.render('Note', {allNotes:allNotes})
+// router.get('/notes', async function(req, res, next){
+//   const allNotes = await notes.find();
+//   res.render('Note', {allNotes:allNotes})
+// })
+
+
+router.get('/Sem-1', async function(req, res, next){
+  const allNote = await notes.find({Semesters:'Semester-1'});
+  res.render('Sem-1', {allNotes:allNote});
 })
+router.get('/Sem-2', async function(req, res, next){
+  const allNote = await notes.find({Semesters:'Semester-2'});
+  res.render('Sem-2', {allNotes:allNote});
+})
+router.get('/Sem-3', async function(req, res, next){
+  const allNote = await notes.find({Semesters:'Semester-3'});
+  res.render('Sem-3', {allNotes:allNote});
+})
+router.get('/Sem-4', async function(req, res, next){
+  const allNote = await notes.find({Semesters:'Semester-4'});
+  res.render('Sem-4', {allNotes:allNote});
+})
+router.get('/Sem-5', async function(req, res, next){
+  const allNote = await notes.find({Semesters:'Semester-5'});
+  res.render('Sem-5', {allNotes:allNote});
+})
+router.get('/Sem-6', async function(req, res, next){
+  const allNote = await notes.find({Semesters:'Semester-6'});
+  res.render('Sem-6', {allNotes:allNote});
+})
+router.get('/Sem-7', async function(req, res, next){
+  const allNote = await notes.find({Semesters:'Semester-7'});
+  res.render('Sem-7', {allNotes:allNote});
+})
+router.get('/Sem-8', async function(req, res, next){
+  const allNote = await notes.find({Semesters:'Semester-8'});
+  res.render('Sem-8', {allNotes:allNote});
+})
+
 module.exports = router;
+
